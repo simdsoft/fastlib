26939992dc126aa9b8637c06e838db4640c50778
halx99 <halx99@live.com>
2020-09-04 11:51:28 +0800

    Tidy and add usage for fastl::fast_csv_parse_line

--- fast_csv.hpp
+++ fast_csv.hpp
@@ -25,118 +25,133 @@
  ****************************************************************************/
 #ifndef SIMDSOFT_FAST_CSV_HPP
 #define SIMDSOFT_FAST_CSV_HPP
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <vector>
+
+#include <type_traits>
+
+/* The usage of fastl::fast_csv_parse_line
+std::string buffer = read_file("xxx.csv");
+const char* cursor = buffer.c_str();
+while(*cursor) {
+   cursor = fastl::fast_csv_parse_line(cursor, [&](const char* v_start, const char* v_end) {
+       // do somethings, store or parse the column value
+       // std::string value(v_start, v_end);
+   });
+}
+*/
 
 namespace fastl {
-    class fast_csv
+    template <typename _Fty, char _VSC> inline const char* fast_xsv_parse_line(const char* s, _Fty&& op);
+
+    /*
+     * op prototype: op(const char* v_start, const char* v_end)
+     */
+    template <typename _Fty> inline const char* fast_csv_parse_line(const char* s, _Fty&& op)
     {
-    public:
-        /*
-         * op prototype: op(const char* v_start, const char* v_end)
-         */
-        template <typename _Fty> inline static const char* parse_line(const char* s, _Fty op)
+        return fast_xsv_parse_line<_Fty, ','>(s, std::forward<_Fty>(op));
+    }
+
+    /*
+     * op prototype: op(const char* v_start, const char* v_end)
+     */
+    template <typename _Fty, char _VSC> inline const char* fast_xsv_parse_line(const char* s, _Fty&& op)
+    {
+        // FSM
+        enum
         {
-            // FSM
-            enum
+            normal, // new field
+            quote_field,
+            quote_field_try_end,
+            quote_field_end,
+        } state;
+
+        state = normal;
+
+        const char* _Start = s; // the start of every string
+        const char* _Ptr = s; // source string iterator
+        const char* _Quote = nullptr;
+        int skipCRLF = 1;
+
+    _L_loop:
+        {
+            switch (*_Ptr)
             {
-                normal, // new field
-                quote_field,
-                quote_field_try_end,
-                quote_field_end,
-            } state;
-
-            state = normal;
-
-            const char* _Start = s; // the start of every string
-            const char* _Ptr = s; // source string iterator
-            const char* _Quote = nullptr;
-            int skipCRLF = 1;
-
-        _L_loop:
-            {
-                switch (*_Ptr)
+            case _VSC:
+                switch (state)
                 {
-                case ',':
-                    switch (state)
-                    {
-                    case normal:
-                        if (_Start <= _Ptr)
-                            op(_Start, _Ptr);
-                        _Start = _Ptr + 1;
-                        break;
-                    case quote_field_try_end:
-                        if (_Start < _Ptr)
-                            op(_Start, _Ptr - 1);
-                    case quote_field_end:
-                        state = normal;
-                        _Start = _Ptr + 1; // skip the field end '\"'
-                        break;
-                    default:; // quote_field, do nothing
-                    }
-
+                case normal:
+                    if (_Start <= _Ptr)
+                        op(_Start, _Ptr);
+                    _Start = _Ptr + 1;
                     break;
-                case '\"':
-                    switch (state)
-                    {
-                    case normal:
-                        state = quote_field;
-                        ++_Start; // skip left '\"'
-                        break;
-                    case quote_field: // field end by right '\"'
-                        _Quote = _Ptr;
-                        state = quote_field_try_end; // delay 1 loop to detect whether is a
-                                                      // inner quote, not end for field
-                        break;
-                    case quote_field_try_end:
-                        if (_Ptr - _Quote > 1)
-                        {
-                            state = quote_field_end;
-                            op(_Start, _Ptr);
-                        }
-                        else
-                        {
-                            state = quote_field;
-                        }
-                        break;
-                    default:;
-                    }
+                case quote_field_try_end:
+                    if (_Start < _Ptr)
+                        op(_Start, _Ptr - 1);
+                case quote_field_end:
+                    state = normal;
+                    _Start = _Ptr + 1; // skip the field end '\"'
                     break;
-                case '\r':
-                    skipCRLF = 2;
-                case '\n':
-                    if (quote_field == state)
-                    {
-                        skipCRLF = 0;
-                        break;
-                    }
-                case '\0':
-                    switch (state)
-                    {
-                    case normal:
-                        if (_Start <= _Ptr)
-                            op(_Start, _Ptr);
-                        break;
-                    case quote_field_try_end:
-                        if (_Start < _Ptr)
-                            op(_Start, _Ptr - 1);
-                        break;
-                    default:; // other state, do nothing, quote_field, invalid csv format
-                    }
-                    goto _L_end;
+                default:; // quote_field, do nothing
                 }
-                ++_Ptr;
 
-                goto _L_loop;
+                break;
+            case '\"':
+                switch (state)
+                {
+                case normal:
+                    state = quote_field;
+                    ++_Start; // skip left '\"'
+                    break;
+                case quote_field: // field end by right '\"'
+                    _Quote = _Ptr;
+                    state = quote_field_try_end; // delay 1 loop to detect whether is a
+                                                  // inner quote, not end for field
+                    break;
+                case quote_field_try_end:
+                    if (_Ptr - _Quote > 1)
+                    {
+                        state = quote_field_end;
+                        op(_Start, _Ptr);
+                    }
+                    else
+                    {
+                        state = quote_field;
+                    }
+                    break;
+                default:;
+                }
+                break;
+            case '\r':
+                skipCRLF = 2;
+            case '\n':
+                if (quote_field == state)
+                {
+                    skipCRLF = 0;
+                    break;
+                }
+            case '\0':
+                switch (state)
+                {
+                case normal:
+                    if (_Start <= _Ptr)
+                        op(_Start, _Ptr);
+                    break;
+                case quote_field_try_end:
+                    if (_Start < _Ptr)
+                        op(_Start, _Ptr - 1);
+                    break;
+                default:; // other state, do nothing, quote_field, invalid csv format
+                }
+                goto _L_end;
             }
+            ++_Ptr;
 
-        _L_end:
-            return _Ptr + skipCRLF; // pointer to next line or after of
-                                    // null-termainted-charactor
+            goto _L_loop;
         }
-    };
+
+    _L_end:
+        return _Ptr + skipCRLF; // pointer to next line or after of
+                                // null-termainted-charactor
+    }
 }
 
 #endif
